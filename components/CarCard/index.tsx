b57e987c5e21e760049a6b10e00a12372163c1eb
Witold <witekrychlik@o2.pl>
2024-03-14 21:30:48 +0100

    style: cars list layout updated

--- components/CarCard/index.tsx
+++ components/CarCard/index.tsx
@@ -1,50 +1,51 @@
 import Link from "next/link";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-  buttonVariants,
-} from "../ui";
+import { Card, CardContent, CardTitle, buttonVariants } from "../ui";
 import { Car } from "../../types";
 import { urlFor } from "../../lib/sanity";
-import Image from "next/image";
 import { lobster } from "../../app/fonts";
 
+import { BsPersonFill, BsCalendar2Fill, BsFuelPumpFill } from "react-icons/bs";
 type CarCardProps = {
   car: Car;
 };
 
 export default function CarCard({ car }: CarCardProps) {
+  const photoURL = urlFor(car.coverImage).url();
   return (
-    <Card className="w-full grid gap-2 overflow-hidden bg-transparent">
-      <CardHeader className="w-full h-[250px] relative ">
-        <Image
-          src={urlFor(car.coverImage).url()}
-          alt={car.model}
-          fill
-          className="rounded-sm"
-        />
-      </CardHeader>
-      <CardContent className="p-4">
+    <Card
+      className="relative w-full h-[350px] grid gap-2 overflow-hidden bg-cover bg-center border-0"
+      style={{ backgroundImage: `url('${photoURL}')` }}
+    >
+      <div className="absolute inset-0 w-full h-full bg-foreground/40 z-10" />
+      <CardContent className="p-4 flex flex-col justify-between z-20">
         <CardTitle
-          className={`${lobster.className} text-left text-secondary tracking-wide`}
+          className={`${lobster.className} text-secondary text-center tracking-wide text-2xl md:text-3xl`}
         >
           {car.model}
         </CardTitle>
+        <div className="w-full flex flex-col gap-2 text-secondary/80 font-bold text-lg">
+          <ul className="w-full flex flex-row gap-5 justify-center">
+            <li className="flex gap-1 items-center">
+              <BsFuelPumpFill /> V8 5L
+            </li>
+            <li className="flex gap-1 items-center">
+              <BsCalendar2Fill /> 1970
+            </li>
+            <li className="flex gap-1 items-center">
+              <BsPersonFill /> 4
+            </li>
+          </ul>
+          <Link
+            href={`/samochody/${car.slug}`}
+            className={buttonVariants({
+              variant: "default",
+              className: "w-full text-primary-foreground font-bold",
+            })}
+          >
+            ZOBACZ PROFIL
+          </Link>
+        </div>
       </CardContent>
-      <CardFooter className="flex justify-center w-full p-4">
-        <Link
-          href={`/samochody/${car.slug}`}
-          className={buttonVariants({
-            variant: "default",
-            className: "w-full text-primary-foreground font-bold",
-          })}
-        >
-          ZOBACZ PROFIL
-        </Link>
-      </CardFooter>
     </Card>
   );
 }
