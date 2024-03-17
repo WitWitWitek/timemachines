6f7c866553e6643aa0c840a3396a99e366d0b830
Witold <witekrychlik@o2.pl>
2024-03-17 17:56:12 +0100

    style: mobile version for promo video

--- app/promo/page.tsx
+++ app/promo/page.tsx
@@ -4,8 +4,8 @@ import { BsYoutube } from "react-icons/bs";
 export default function page() {
   return (
     <Section className="bg-foreground h-screen min-h-screen-[unset]">
-      <Wrapper className="w-full h-full">
-        <div className="relative w-full h-full">
+      <Wrapper className="w-full h-full flex">
+        <div className="relative hidden md:block w-full h-full">
           <iframe
             src="https://www.youtube.com/embed/8F-WjCOr6Io?si=qCtvvWnMsj3axKW-"
             title="Embedded youtube"
@@ -15,10 +15,19 @@ export default function page() {
             className="absolute inset-0 md:h-full w-full md:max-w-full aspect-video m-auto rounded-sm"
           />
         </div>
-        <div className="w-full md:hidden flex justify-center">
+        <div className="w-full md:hidden flex flex-col justify-center items-center gap-5">
+          <p className="text-3xl text-secondary text-center">
+            Wideo promocyjne nie jest dostępne w wersji na telefon. Przejdz do
+            aplikacji <span className="text-primary font-bold">YouTube</span>, z
+            naszym filmem promocyjnym:
+          </p>
           <Link
             href="https://youtu.be/8F-WjCOr6Io?feature=shared"
-            className={buttonVariants({ variant: "default" })}
+            className={buttonVariants({
+              variant: "default",
+              className: "w-full",
+            })}
+            target="_blank"
           >
             Zobacz promo w serwisie
             <span className="flex flex-row gap-1 items-center ml-1 font-bold">
